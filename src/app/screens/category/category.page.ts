e34a5f85195322ade4a74dd79238f876130809ba
kevin alexander soto <kevinalexandersoto999@gmail.com>
2023-12-05 14:42:11 -0500

    feature: crate categoty head lines

--- src/app/screens/category/category.page.ts
+++ src/app/screens/category/category.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SegmentChangeEventDetail } from '@ionic/angular';
+import { IonSegmentCustomEvent } from '@ionic/core';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryPage implements OnInit {
 
+  public categories: String[] = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+
+  public initialSelectecCategory: String = this.categories[0];
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.categories);
+  }
+
+  onSegmentChanged(event: IonSegmentCustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail.value);
   }
 
 }
