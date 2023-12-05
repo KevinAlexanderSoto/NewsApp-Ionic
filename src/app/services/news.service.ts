570cce73d78d95130fd4e3a7de07e6480bec5847
kevin alexander soto <kevinalexandersoto999@gmail.com>
2023-12-05 14:41:40 -0500

    feature: creaye get news by category service method

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -24,4 +24,14 @@ export class NewsService {
       map(res => res.articles)
     );
   }
+  getNewsByCategory(category: string): Observable<Article[]> {
+    return this.httpClient.get<NewsResponse>(`${mainUrl}top-headlines?country=us&category=${category}`, {
+      params: {
+        apiKey: apiKey,
+      }
+    }).pipe(
+      map(res => res.articles)
+    );
+  }
+
 }
